df69b199dec46dd927d76f9bebd36719798e9cfc
weijiehiew <weijiehiew@gmail.com>
2023-01-25 13:49:52 +0800

    Updated

---
+++ hhhhhhhhh.py
@@ -0,0 +1,27 @@
+if i==1:
+    time.sleep(10)
+    play(t1)
+elif i == 2:
+    time.sleep(10)
+    play(t2)
+elif i == 3:
+    time.sleep(10)
+    play(t4)
+    play(t5)
+elif i == 4:
+    time.sleep(10)
+    play(t6)
+elif i == 5:
+    time.sleep(10)
+    play(t8)
+elif i == 9:
+    time.sleep(10)
+    play(t9)
+elif i == 12:
+    time.sleep(10)
+    play(t10)
+elif i == 14:
+    time.sleep(10)
+    play(t11)
+else:
+    time.sleep(15)
