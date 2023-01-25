89a2992cece9299daf32ffd10311e2af57be7139
weijiehiew <weijiehiew@gmail.com>
2023-01-25 09:48:07 +0800

    sendtopolarizer update

---
+++ sendtopolarizer.py
@@ -0,0 +1,41 @@
+from tkinter import *
+from PIL import Image, ImageOps
+from student_pub import *
+
+def middleman(Images):
+	#print(Image)
+	myImage = Image.open(Images)
+	## greyscale image file
+	greyImage = ImageOps.grayscale(myImage)
+	#greyImage.show()
+
+	## Limiting to 8 shades of greyscale colour
+	greyQuantize = greyImage.quantize(8)
+	#greyQuantize.show()
+
+	## resize to 32 x 32 pixels
+	smallImage = greyQuantize.resize((32,32), Image.BILINEAR)
+	#smallImage.show()
+
+	## Blow it back up to original photo size (32 x 32 pixels upscale)
+	resultImage = smallImage.resize(myImage.size, Image.NEAREST)
+	#resultImage.show()
+
+	## Write Image to save .png file
+	resultImage.save('cartoon.png')
+
+	# Retrieving pixel value and formating it into list of list
+	x = 32; k = 0; outputValue = [0 for i in range(x)]
+	for i in range(x):
+  	  outputValue[i] = [0 for j in range(x)]
+	pixValue = list(smallImage.getdata())
+
+	for i in range(x):
+   	 for j in range(x):
+	        outputValue[i][j] = pixValue[k]
+	        k = k + 1;
+
+	print(outputValue)
+	pubpic(outputValue)
+		
+
